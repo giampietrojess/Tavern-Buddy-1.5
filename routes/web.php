d6a77b9d206aef84e03be708d1355087433c9faf
Hillary Lavin <hilavin@gmail.com>
2018-07-20 12:07:47 -0400

    Remove old routes to create/show characters

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/addCharacter', function () {
-    return view('addCharacter');
-});
-
-Route::get('/CharacterSheet', function () {
-    return view('CharacterSheet');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
