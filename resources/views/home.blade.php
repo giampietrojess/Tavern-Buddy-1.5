899e64db123c34bb7e7be9757687ea1a9efef95f
Jess Giampietro <giampietrojess@gmail.com>
2018-07-19 20:52:35 -0400

    add full page jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="jumbotron text-center">
+    <img class="img-fluid" src="./img/TBHeader.png">   
+        <h5>Welcome {{ Auth::user()->name }}! Your Adventure Begins Now!</h5>
+</div>
+<div class="container text-center">
+    
+    
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
