6ffa59f24fa6205fa3c856f073e818608592369b
Jess Giampietro <giampietrojess@gmail.com>
2018-07-19 22:14:23 -0400

    remove image from dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="jumbotron text-center">
-    <img class="img-fluid" src="./img/TBHeader.png">   
-        <h5>Welcome {{ Auth::user()->name }}! Your Adventure Begins Now!</h5>
-</div>
-<div class="container text-center">
-    
-    
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container">
+    <div class="jumbotron text-center">
+        
+            <h2>Welcome {{ Auth::user()->name }}! Your Adventure Begins Now!</h2>
+    </div>
+    <div class="container text-center">
+        
+        
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
