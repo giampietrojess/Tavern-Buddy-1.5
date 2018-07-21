b0e128c5f4e834825099a3e0a732f0e2ed619ef1
Jess Giampietro <giampietrojess@gmail.com>
2018-07-21 09:10:33 -0400

    add a home button to navbar if the user is logged in

--- resources/views/inc/Navbar.blade.php
+++ resources/views/inc/Navbar.blade.php
@@ -31,6 +31,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                     <a class="dropdown-item" href="/dashboard">
+                                        Home
+                                    </a>
+                                
+                                
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
