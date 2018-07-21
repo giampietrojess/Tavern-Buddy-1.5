4fb14e9b07517ccc674b2859d0b2cfce3710a021
Jess Giampietro <giampietrojess@gmail.com>
2018-07-21 09:47:42 -0400

    attempt to add form

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -5,6 +5,12 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <h1>Add a Character questions</h1>
+                {!! Form::open(['url' => 'characters/create']) !!}
+                    <div class="form-group">
+                        {{Form::label('title', 'Title')}}
+                        {{Form::text('title', 'class', => )}}
+                    </div>
+                {!! Form::close() !!}
             </div>
         </div> 
     </div>
