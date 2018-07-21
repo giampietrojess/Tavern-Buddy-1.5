ee3a0b316ac36779f5b3961c1d079f3bba2b89c4
Jess Giampietro <giampietrojess@gmail.com>
2018-07-21 18:11:45 -0400

    Add user name to player name space on character sheet

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="container">
-    <h1>View All My Characters</h1>
+    <h1>View EVERYONE'S Characters</h1>
         @if(count($characters) > 0)
             @foreach($characters as $character)
                 <div class="card">
-                    <h3>{{$character->character_name}}</h3>
+                    <h3><a href="/characters/{{$character->id}}">{{$character->character_name}}</a></h3>
                 </div> 
             @endforeach
         @else
