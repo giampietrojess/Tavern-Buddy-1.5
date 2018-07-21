8e18c488296e91ca0be4b52413b177d9f65439dc
Hillary Lavin <hilavin@gmail.com>
2018-07-21 16:25:01 -0400

    Create belongsTo/hasMany relationship between Character and User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function characters() {
+        return $this->hasMany('App\Character');
+    }
 }
