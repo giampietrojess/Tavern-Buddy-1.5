119abe859ce6ed422840069a7f0c58fe2469bb9a
Jess Giampietro <giampietrojess@gmail.com>
2018-07-21 09:10:23 -0400

    Apply basic html styling for create blade

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-   Add a Character questions
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Add a Character questions</h1>
+            </div>
+        </div> 
+    </div>
 @endsection
