38f5d88c2c5046d65aeb19a3e1dd7e67ff04f627
Jess Giampietro <giampietrojess@gmail.com>
2018-07-21 19:28:41 -0400

    add attributes coding

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -15,6 +15,56 @@
                 <div class="xpBox">X</div>
                 <div class="userBox">{{$character->user->name}}</div>
             </div>
+
+            <div class="attributesHere">
+                <div class="attBox">
+                    Strength
+                    <div class="col-md-12 text-center score">
+                        {{$character->str_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+
+                <div class="attBox">
+                    Dexterity
+                    <div class="col-md-12 text-center score">
+                        {{$character->dex_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+
+                <div class="attBox">
+                    Constitution
+                    <div class="col-md-12 text-center score">
+                        {{$character->con_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+
+                <div class="attBox">
+                    Intelligence
+                    <div class="col-md-12 text-center score">
+                        {{$character->int_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+
+                <div class="attBox">
+                    Wisdom
+                    <div class="col-md-12 text-center score">
+                        {{$character->wis_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+
+                <div class="attBox">
+                    Charisma
+                    <div class="col-md-12 text-center score">
+                        {{$character->cha_score}}
+                    </div>
+                    <div class="modifier">+?</div>
+                </div>
+            </div>
         </div>
     </div>
     
