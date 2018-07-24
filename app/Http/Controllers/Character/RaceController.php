0f8241d6ddc784ab60101141cb5202a61f726975
Hillary Lavin <hilavin@gmail.com>
2018-07-24 12:42:58 -0400

    Remove commented out if/else from multi-page form tutorial

--- app/Http/Controllers/Character/RaceController.php
+++ app/Http/Controllers/Character/RaceController.php
@@ -26,17 +26,10 @@ class RaceController extends Controller
             'race' => 'required'
         ]);
         $character = $request->session()->get('character');
-        // if(empty($request->session()->get('character'))){
-        //     $character = new Character();
-        //     $character->race = $request->input('race');
-        //     $character->user_id = auth()->user()->id;
-        //     $request->session()->put('character', $character);
-        // }else{
-            // $character = $request->session()->get('character');
-            $character->race = $request->input('race');
-            $character->user_id = auth()->user()->id;
-            $request->session()->put('character', $character);
-        // }
+        $character->race = $request->input('race');
+        $character->user_id = auth()->user()->id;
+        $request->session()->put('character', $character);
+
         
         return redirect('/classQuest')->with('success', 'Race Saved!');
     }
