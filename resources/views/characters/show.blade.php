19fe3f6b66784f32e281d4c392de7a5819afdb27
Jess Giampietro <giampietrojess@gmail.com>
2018-07-24 00:57:16 -0400

    add new boxes to character sheet

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -20,7 +20,9 @@
             </div>
                 
             <div class="col-md-3">    
-                <!-- If the user who is viewing the character is the one who created the character, show edit and delete buttons -->
+                <!-- If the user who is viewing the character is 
+                the one who created the character, show 
+                edit and delete buttons -->
                 @if(!Auth::guest())
                     @if(Auth::user()->id == $character->user_id)
                         <!-- Pencil Link -->
@@ -60,71 +62,113 @@
 
         <div class="row">
 
-        <div class="container">
-            <div class="characterSheetHead">
-                <div class="nameBox">{{$character->character_name}}</div>
-                <div class="raceBox">{{$character->race}}</div>
-                <div class="classBox">{{$character->class}}</div>
-                <div class="levelBox">X</div>
-                <div class="bgBox">{{$character->background}}</div>
-                <div class="alignmentBox">{{$character->alignment}}</div>
-                <div class="xpBox">X</div>
-                <div class="userBox">{{$character->user->name}}</div>
+            <div class="container">
+                <div class="characterSheetHead">
+                    <div class="nameBox">{{$character->character_name}}</div>
+                    <div class="raceBox">{{$character->race}}</div>
+                    <div class="classBox">{{$character->class}}</div>
+                    <div class="levelBox">X</div>
+                    <div class="bgBox">{{$character->background}}</div>
+                    <div class="alignmentBox">{{$character->alignment}}</div>
+                    <div class="xpBox">X</div>
+                    <div class="userBox">{{$character->user->name}}</div>
+                </div>
+
+                <div class="attributesHere">
+                    <div class="attBox">
+                        Strength
+                        <div class="col-md-12 text-center score">
+                            {{$character->str_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+
+                    <div class="attBox">
+                        Dexterity
+                        <div class="col-md-12 text-center score">
+                            {{$character->dex_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+
+                    <div class="attBox">
+                        Constitution
+                        <div class="col-md-12 text-center score">
+                            {{$character->con_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+
+                    <div class="attBox">
+                        Intelligence
+                        <div class="col-md-12 text-center score">
+                            {{$character->int_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+
+                    <div class="attBox">
+                        Wisdom
+                        <div class="col-md-12 text-center score">
+                            {{$character->wis_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+
+                    <div class="attBox">
+                        Charisma
+                        <div class="col-md-12 text-center score">
+                            {{$character->cha_score}}
+                        </div>
+                        <div class="modifier">+?</div>
+                    </div>
+                
+                </div>
+
             </div>
-
-            <div class="attributesHere">
-                <div class="attBox">
-                    Strength
-                    <div class="col-md-12 text-center score">
-                        {{$character->str_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-
-                <div class="attBox">
-                    Dexterity
-                    <div class="col-md-12 text-center score">
-                        {{$character->dex_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-
-                <div class="attBox">
-                    Constitution
-                    <div class="col-md-12 text-center score">
-                        {{$character->con_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-
-                <div class="attBox">
-                    Intelligence
-                    <div class="col-md-12 text-center score">
-                        {{$character->int_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-
-                <div class="attBox">
-                    Wisdom
-                    <div class="col-md-12 text-center score">
-                        {{$character->wis_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-
-                <div class="attBox">
-                    Charisma
-                    <div class="col-md-12 text-center score">
-                        {{$character->cha_score}}
-                    </div>
-                    <div class="modifier">+?</div>
-                </div>
-            </div>
-
-
-
         </div>
+
+
+        <div class='row'>
+
+            <div class="col-md-4">Something</div>
+            
+            <div class="HPcontainer">
+
+                <div class="row">
+
+                    <div class="initiative">
+                        Initiative
+                    </div>
+
+                    <div class="armorClass">
+                        Armor<br>Class
+                    </div>
+
+                    <div class="speedBox">
+                        speed
+                    </div>
+
+                
+                </div>
+                <div class="row">
+                    <div class="currentHP">
+                            Current Hit Points
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="tempHP">
+                        Temporary Hit Points
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-3">Something</div>
+              
+            
+        </div>
+
     </div>
     
 @endsection
