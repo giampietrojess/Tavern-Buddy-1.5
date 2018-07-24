f836379e303ced94d87ba8f49cb45cd56450ffbe
Jess Giampietro <giampietrojess@gmail.com>
2018-07-23 21:11:05 -0400

    remove unnecessary if/else statements from views

--- app/Http/Controllers/Character/AbilityController.php
+++ app/Http/Controllers/Character/AbilityController.php
@@ -17,17 +17,17 @@ class AbilityController extends Controller
     public function postabilityQuest(Request $request)
     {
         $character = $request->session()->get('character');
-        if(empty($request->session()->get('character'))){
-            $character = new Character();
-            $character->str_score = $request->input('str_score');
-            $character->dex_score = $request->input('dex_score');
-            $character->con_score = $request->input('con_score');
-            $character->int_score = $request->input('int_score');
-            $character->wis_score = $request->input('wis_score');
-            $character->cha_score = $request->input('cha_score');
-            $character->user_id = auth()->user()->id;
-            $request->session()->put('character', $character);
-        }else{
+        // if(empty($request->session()->get('character'))){
+        //     $character = new Character();
+        //     $character->str_score = $request->input('str_score');
+        //     $character->dex_score = $request->input('dex_score');
+        //     $character->con_score = $request->input('con_score');
+        //     $character->int_score = $request->input('int_score');
+        //     $character->wis_score = $request->input('wis_score');
+        //     $character->cha_score = $request->input('cha_score');
+        //     $character->user_id = auth()->user()->id;
+        //     $request->session()->put('character', $character);
+        // }else{
             $character = $request->session()->get('character');
             $character->str_score = $request->input('str_score');
             $character->dex_score = $request->input('dex_score');
@@ -37,7 +37,7 @@ class AbilityController extends Controller
             $character->cha_score = $request->input('cha_score');
             $character->user_id = auth()->user()->id;
             $request->session()->put('character', $character);
-        }
+        // }
         
     return redirect('/backgroundQuest')->with('success', 'Abilities Saved!');
     }
