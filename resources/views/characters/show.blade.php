386605531e6e8c2e72cb1e702498360c74d971e9
Jess Giampietro <giampietrojess@gmail.com>
2018-07-24 19:57:01 -0400

    rearrange coding to make character sheet more responsive

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -124,119 +124,107 @@
                     </div>
                 
                 </div>
-
-            </div>
-        </div>
-
-
-        <div class='row'>
-
-            <div class="col-md-4">
-                <div class="row">
-                    
-                    <div class="proficiency">
-                        <div class="row">
-                            <div class="col-md-3 text-center proficiencyScore">
-                            ?
-                            </div>
-                            <div class="col-md-9 text-center">
-                                Proficiency Bonus
+                
+                    <div class="profskiCon">   
+                        <div class="proficiency">
+                            <div class="row">
+                                <div class="col-md-3 text-center proficiencyScore">
+                                ?
+                                </div>
+                                <div class="col-md-9 text-center">
+                                    Proficiency Bonus
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </div>
-                <div class="row">
-                    <div class="skills">
-                        
-                        <h5><u>Strength Skills</u></h5>
-                     
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Athletics:  ___</p>
-                        <hr>
-                        <h5><u>Dexterity Skills</u></h5>
-                
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Acrobatics:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Sleight of Hand:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Stealth:  ___</p>
-                        <hr>
-                        <h5><u>Constitution Skills</u></h5>
-                        
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <hr>
-                        <h5><u>Intelligence Skills</u></h5>
+
+                        <div class="skills">
+                            
+                            <h5><u>Strength Skills</u></h5>
+                            
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Athletics:  ___</p>
+                            <hr>
+                            <h5><u>Dexterity Skills</u></h5>
                     
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Arcana:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> History:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Investigation:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Nature:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Religion:  ___</p>
-                        <hr>
-                        <h5><u>Wisdom Skills</u></h5>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Acrobatics:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Sleight of Hand:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Stealth:  ___</p>
+                            <hr>
+                            <h5><u>Constitution Skills</u></h5>
+                            
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <hr>
+                            <h5><u>Intelligence Skills</u></h5>
                         
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Animal Handling:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Insight:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Medicine:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Perception:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Survival:  ___</p>
-                        <hr>
-                        <h5><u>Charisma Skills</u></h5>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Arcana:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> History:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Investigation:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Nature:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Religion:  ___</p>
+                            <hr>
+                            <h5><u>Wisdom Skills</u></h5>
+                            
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Animal Handling:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Insight:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Medicine:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Perception:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Survival:  ___</p>
+                            <hr>
+                            <h5><u>Charisma Skills</u></h5>
+                        
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Deception:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Intimidation:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Performance:  ___</p>
+                            <p class="skilly"><i class="fa fa-circle-thin"></i> Persuasion:  ___</p>
+                            <hr>
+                        </div>
+                    </div>  
+
+                <div class="HPcontainer">
+
+                    <div class="row">
+
+                        <div class="initiative">
+                            Initiative
+                        </div>
+
+                        <div class="armorClass">
+                            Armor<br>Class
+                        </div>
+
+                        <div class="speedBox">
+                            speed
+                        </div>
+
                     
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Saving Throws: ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Deception:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Intimidation:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Performance:  ___</p>
-                        <p class="skilly"><i class="fa fa-circle-thin"></i> Persuasion:  ___</p>
-                        <hr>
+                    </div>
+                    <div class="row">
+                        <div class="currentHP">
+                                Current Hit Points
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="tempHP">
+                            Temporary Hit Points
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="hitDice">
+                            Hit Dice
+                        </div>
+                        <div class="hitDice">
+                            Death Saves
+                        </div>
+                        
                     </div>
                 </div>
 
-            </div>
-            
-            <div class="HPcontainer">
-
-                <div class="row">
-
-                    <div class="initiative">
-                        Initiative
-                    </div>
-
-                    <div class="armorClass">
-                        Armor<br>Class
-                    </div>
-
-                    <div class="speedBox">
-                        speed
-                    </div>
-
-                
-                </div>
-                <div class="row">
-                    <div class="currentHP">
-                            Current Hit Points
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="tempHP">
-                        Temporary Hit Points
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="hitDice">
-                        Hit Dice
-                    </div>
-                    <div class="hitDice">
-                        Death Saves
-                    </div>
                     
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                
                 <div class="inventoryContainer text-center">
             
                     <img class="iconR" src="/img/weaponicon.png" alt="weapon"><div class="head">Weapons</div>
@@ -250,6 +238,11 @@
                         Text text text text text text text text text text text text text text
 
                     </div>
+
+        
+                </div>
+
+                <div class="extraBox">
                     <img class="iconL" src="/img/inventoryicon.png" alt="bag"><div class="head">Inventory</div>
                     <div class="inner clearfix">
                             
@@ -261,7 +254,9 @@
                         Text text text text text text text text text text text text text text
 
                     </div>
-                    <img class="iconR" src="/img/moneyIcon.png" alt="money"><div class="head">Money Pouch</div>
+                </div>
+                <div class="extraBox">
+                <img class="iconR" src="/img/moneyIcon.png" alt="money"><div class="head">Money Pouch</div>
                     <div class="inner clearfix">
                             
                         Text text text text text text text text text text text text text text
@@ -272,6 +267,8 @@
 
                     </div>
                 </div>
+
+                
             </div>
               
             
