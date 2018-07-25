be6404e1956bee6a6658ae24dcb468cf9aa2bf5f
Jess Giampietro <giampietrojess@gmail.com>
2018-07-25 00:33:07 -0400

    add modifier code to update controller

--- app/Http/Controllers/Character/CharactersController.php
+++ app/Http/Controllers/Character/CharactersController.php
@@ -115,11 +115,169 @@ class CharactersController extends Controller
         $character->race = $request->input('race');
         $character->class = $request->input('class');
         $character->str_score = $request->input('str_score');
+            switch ($character->str_score) {
+                case 8:
+                case 9:
+                    $character->str_mod = -1;
+                    break;
+                case 10: 
+                case 11:
+                    $character->str_mod = 0;
+                    break;
+                case 12: 
+                case 13:
+                    $character->str_mod = 1;
+                    break;
+                case 14: 
+                case 15:
+                    $character->str_mod = 2;
+                    break;
+                case 16: 
+                case 17:
+                    $character->str_mod = 3;
+                    break;
+                case 18:
+                case 19:
+                    $character->str_mod = 4;
+                    break;
+            }
         $character->dex_score = $request->input('dex_score');
+            switch ($character->dex_score) {
+                case 8:
+                case 9:
+                    $character->dex_mod = -1;
+                    break;
+                case 10: 
+                case 11:
+                    $character->dex_mod = 0;
+                    break;
+                case 12: 
+                case 13:
+                    $character->dex_mod = 1;
+                    break;
+                case 14: 
+                case 15:
+                    $character->dex_mod = 2;
+                    break;
+                case 16: 
+                case 17:
+                    $character->dex_mod = 0;
+                    break;
+                case 18:
+                case 19:
+                    $character->dex_mod = 4;
+                    break;
+            }
         $character->con_score = $request->input('con_score');
+            switch ($character->con_score) {
+                case 8:
+                case 9:
+                    $character->con_mod = -1;
+                    break;
+                case 10: 
+                case 11: 
+                    $character->con_mod = 0;
+                    break;
+                case 12: 
+                case 13: 
+                    $character->con_mod = 1;
+                    break;
+                case 14: 
+                case 15: 
+                    $character->con_mod = 2;
+                    break;
+                case 16: 
+                case 17: 
+                    $character->con_mod = 3;
+                    break;
+                case 18:
+                case 19:
+                    $character->con_mod = 4;
+                    break;
+            }
         $character->int_score = $request->input('int_score');
+            switch ($character->int_score) {
+                case 8:
+                case 9:
+                    $character->int_mod = -1;
+                    break;
+                case 10: 
+                case 11:
+                    $character->int_mod = 0;
+                    break;
+                case 12: 
+                case 13:
+                    $character->int_mod = 1;
+                    break;
+                case 14: 
+                case 15:
+                    $character->int_mod = 2;
+                    break;
+                case 16: 
+                case 17:
+                    $character->int_mod = 3;
+                    break;
+                case 18:
+                case 19:
+                    $character->int_mod = 4;
+                    break;
+            }
         $character->wis_score = $request->input('wis_score');
+        switch ($character->wis_score) {
+            case 8:
+            case 9:
+                $character->wis_mod = -1;
+                break;
+            case 10: 
+            case 11:
+                $character->wis_mod = 0;
+                break;
+            case 12: 
+            case 13:
+                $character->wis_mod = 1;
+                break;
+            case 14: 
+            case 15:
+                $character->wis_mod = 2;
+                break;
+            case 16: 
+            case 17:
+                $character->wis_mod = 3;
+                break;
+            case 18:
+            case 19:
+                $character->wis_mod = 4;
+                break;
+        }
+
         $character->cha_score = $request->input('cha_score');
+            switch ($character->cha_score) {
+                case 8:
+                case 9:
+                    $character->cha_mod = -1;
+                    break;
+                case 10: 
+                case 11:
+                    $character->cha_mod = 0;
+                    break;
+                case 12: 
+                case 13:
+                    $character->cha_mod = 1;
+                    break;
+                case 14: 
+                case 15:
+                    $character->cha_mod = 2;
+                    break;
+                case 16: 
+                case 17:
+                    $character->cha_mod = 3;
+                    break;
+                case 18:
+                case 19:
+                    $character->cha_mod = 4;
+                    break;
+            }
+
         $character->background = $request->input('background');
         $character->alignment = $request->input('alignment');
         $character->save();
