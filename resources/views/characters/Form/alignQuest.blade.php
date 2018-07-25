c7a73516be112189983deb53e122ef07063f922e
Jess Giampietro <giampietrojess@gmail.com>
2018-07-25 03:05:47 -0400

    add carousel to align blade

--- resources/views/characters/Form/alignQuest.blade.php
+++ resources/views/characters/Form/alignQuest.blade.php
@@ -3,44 +3,168 @@
 @section('content')
 <div class="container text-center">
     <h1>What is your Character's Alignment?</h1>
-    
-    {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        {{ csrf_field() }}
-        <div class="form-group">
-            <h3>{{Form::label('What is your Character\'s Alignment?')}}</h3>
-            <p>
-            {{Form::label('Lawful Good')}}
-            {{Form::radio('alignment', 'Lawful Good', true, ['class' => 'radio'])}}
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                {{ csrf_field() }}
+                <div id="carouselExampleControls" class="carousel slide">
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-md-4"></div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="First slide">
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                            <p>
+                                {{Form::label('Lawful Good')}}
+                                {{Form::radio('alignment', 'Lawful Good', false, ['class' => 'radio'])}}
+                            </p>
+                        </div>
+                        <div class="carousel-item">
+                        <div class="row">
+                                <div class="col-md-4"></div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Second slide">
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                            <p>                   
+                                {{Form::label('Neutral Good')}}
+                                {{Form::radio('alignment', 'Neutral Good', false, ['class' => 'radio radio-inline'])}}
+                            </p>
+                        </div>
+                        <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Third slide">
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 3</p>
+                        </div>
+                        <div class="carousel-item">
+                            
+                            <div class="row">
+                                <div class="col-md-4"></div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Fourth slide">
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 4</p>    
+                        
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Fifth slide">
+                            </div>
+                            <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 5</p>
+                            
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Sixth slide">
+                            </div>
+                            <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 6</p>
+                        
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Seventh slide">
+                            </div>
+                            <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 7</p>
+                        </div>
+                        <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Eigth slide">
+                            </div>
+                            <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 8</p>
+                        </div>
+                        <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Ninth slide">
+                            </div>
+                            <div class="col-md-4"></div>
+                            </div>
+                            <p>Test 9</p>
+                        </div>
+                    </div>  
 
-            {{Form::label('Neutral Good')}}
-            {{Form::radio('alignment', 'Neutral Good', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Chaotic Good')}}
-            {{Form::radio('alignment', 'Chaotic Good',  ['class' => 'radio radio-inline'])}}
-
-                {{Form::label('Lawful Neutral')}}
-            {{Form::radio('alignment', 'Lawful Neutral', true, ['class' => 'radio'])}}
-
-            {{Form::label('True Neutral')}}
-            {{Form::radio('alignment', 'True Neutral', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Chaotic Neutral')}}
-            {{Form::radio('alignment', 'Chaotic Neutral', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Lawful Evil')}}
-            {{Form::radio('alignment', 'Lawful Evil', true, ['class' => 'radio'])}}
-
-            {{Form::label('Neutral Evil')}}
-            {{Form::radio('alignment', 'Neutral Evil', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Chaotic Evil')}}
-            {{Form::radio('alignment', 'Chaotic Evil',  ['class' => 'radio radio-inline'])}}
-
-            </p>
-            <hr>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
+            {!! Form::close() !!}      
         </div>
-        {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
+    
+        <div class="col-md-3"></div>
+            {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <h3>{{Form::label('What is your Character\'s Alignment?')}}</h3>
+                    <p>
+                    {{Form::label('Lawful Good')}}
+                    {{Form::radio('alignment', 'Lawful Good', false, ['class' => 'radio'])}}
 
+                    {{Form::label('Neutral Good')}}
+                    {{Form::radio('alignment', 'Neutral Good', false, ['class' => 'radio radio-inline'])}}
 
+                    {{Form::label('Chaotic Good')}}
+                    {{Form::radio('alignment', 'Chaotic Good', false, ['class' => 'radio radio-inline'])}}
+
+                    {{Form::label('Lawful Neutral')}}
+                    {{Form::radio('alignment', 'Lawful Neutral', false, ['class' => 'radio'])}}
+
+                    {{Form::label('True Neutral')}}
+                    {{Form::radio('alignment', 'True Neutral', false, ['class' => 'radio radio-inline'])}}
+
+                    {{Form::label('Chaotic Neutral')}}
+                    {{Form::radio('alignment', 'Chaotic Neutral', false, ['class' => 'radio radio-inline'])}}
+
+                    {{Form::label('Lawful Evil')}}
+                    {{Form::radio('alignment', 'Lawful Evil', false, ['class' => 'radio'])}}
+
+                    {{Form::label('Neutral Evil')}}
+                    {{Form::radio('alignment', 'Neutral Evil', false, ['class' => 'radio radio-inline'])}}
+
+                    {{Form::label('Chaotic Evil')}}
+                    {{Form::radio('alignment', 'Chaotic Evil', false, ['class' => 'radio radio-inline'])}}
+
+                    </p>
+                    <hr>
+                </div>
+                {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+</div>    
 @endsection
--- resources/views/characters/Form/classQuest.blade.php
+++ resources/views/characters/Form/classQuest.blade.php
@@ -1,63 +1,108 @@
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container text-center">
-    <h1>What is your Character's Class?</h1>
     
-    {!! Form::open(['action' => 'Character\ClassController@index', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        {{ csrf_field() }}
+    
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+        
+            {!! Form::open(['action' => 'Character\ClassController@index', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                {{ csrf_field() }}
 
-        <!-- Character Class -->
-        <div class="form-group">
-            <h3>{{Form::label('What Class is your Character?')}}</h3>
-            <p>
-            {{Form::label('Barbarian')}}
-            {{Form::radio('class', 'Barbarian', ['class' => 'radio'])}}
+                <!-- Character Class -->
+                <div class="form-group">
+                    <h1>{{Form::label('What Class is your Character?')}}</h1>
+                    <p>
+                        <div class="container">   
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png">
+                                    <p>{{Form::label('Barbarian')}}
+                                    {{Form::radio('class', 'Barbarian', false, ['class' => 'radio'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/BardIcon.png">
+                                    <p>{{Form::label('Bard')}}
+                                    {{Form::radio('class', 'Bard', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/ClericIcon.png">
+                                    <p>{{Form::label('Cleric')}}
+                                    {{Form::radio('class', 'Cleric', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/DruidIcon.png">
+                                    <p>{{Form::label('Druid')}}
+                                    {{Form::radio('class', 'Druid', false, ['class' => 'radio radio-inline'])}} </p>
+                                </div>
+                            </div>
 
-            {{Form::label('Bard')}}
-            {{Form::radio('class', 'Bard', ['class' => 'radio radio-inline'])}}
+                            <div class="row">
 
-            {{Form::label('Cleric')}}
-            {{Form::radio('class', 'Cleric', ['class' => 'radio radio-inline'])}}
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/FighterIcon.png">
+                                    <p>{{Form::label('Fighter')}}
+                                    {{Form::radio('class', 'Fighter', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/MonkIcon.png">
+                                    <p>{{Form::label('Monk')}}
+                                    {{Form::radio('class', 'Monk', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/Paladin.png">
+                                    <p>{{Form::label('Paladin')}}
+                                    {{Form::radio('class', 'Paladin', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/RangerIcon.png">
+                                    <p>{{Form::label('Ranger')}}
+                                    {{Form::radio('class', 'Ranger', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                            </div>
 
-            {{Form::label('Druid')}}
-            {{Form::radio('class', 'Druid', ['class' => 'radio radio-inline'])}}
 
-            {{Form::label('Fighter')}}
-            {{Form::radio('class', 'Fighter', ['class' => 'radio radio-inline'])}}
+                            <div class="row">
 
-            {{Form::label('Monk')}}
-            {{Form::radio('class', 'Monk', ['class' => 'radio radio-inline'])}}
+                                <div class="col-md-3">
+                                    <img class="img-fluid" class="img-fluid" src="/img/ClassIcons/RogueIcon.png">
+                                    <p>{{Form::label('Rogue')}}
+                                    {{Form::radio('class', 'Rogue', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/SorcererIcon.png">
+                                    <p>{{Form::label('Sorcerer')}}
+                                    {{Form::radio('class', 'Sorcerer', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/WarlockIcon.png">
+                                    <p>{{Form::label('Warlock')}}
+                                    {{Form::radio('class', 'Warlock', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <img class="img-fluid" src="/img/ClassIcons/WizardIcon.png">
+                                    <p>{{Form::label('Wizard')}}
+                                    {{Form::radio('class', 'Wizard', false, ['class' => 'radio radio-inline'])}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </p>
 
-            {{Form::label('Paladin')}}
-            {{Form::radio('class', 'Paladin', ['class' => 'radio radio-inline'])}}
+                    <hr>          
+                </div>
 
-            {{Form::label('Ranger')}}
-            {{Form::radio('class', 'Ranger', ['class' => 'radio radio-inline'])}}
+                {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
+            {!! Form::close() !!}
 
-            {{Form::label('Rogue')}}
-            {{Form::radio('class', 'Rogue', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Sorcerer')}}
-            {{Form::radio('class', 'Sorcerer', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Warlock')}}
-            {{Form::radio('class', 'Warlock', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Rogue')}}
-            {{Form::radio('class', 'Rogue', ['class' => 'radio radio-inline'])}}
-
-            {{Form::label('Wizard')}}
-            {{Form::radio('class', 'Wizard', ['class' => 'radio radio-inline'])}}
-            </p>
-            <hr>
         </div>
-
-        {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
-
-    <div>{{$barbarian->name}} | {{$bard->name}} | {{$cleric->name}} | {{$druid->name}} | {{$fighter->name}} | {{$monk->name}} | {{$paladin->name}} | {{$ranger->name}} | {{$rogue->name}} | {{$sorcerer->name}} | {{$warlock->name}} | {{$wizard->name}}</div>
-   
-
+        <div class="col-md-2"></div>
+          <!-- <div>{{$barbarian->name}} | {{$bard->name}} | {{$cleric->name}} | {{$druid->name}} | {{$fighter->name}} | {{$monk->name}} | {{$paladin->name}} | {{$ranger->name}} | {{$rogue->name}} | {{$sorcerer->name}} | {{$warlock->name}} | {{$wizard->name}}</div>
+    -->
+    </div>
+</div>
 
 @endsection
