f09a58cdae66254a0d1f746996b78c53d92648ef
Hillary Lavin <hilavin@gmail.com>
2018-07-25 15:15:38 -0400

    Add raceArray to controller

--- app/Http/Controllers/Character/RaceController.php
+++ app/Http/Controllers/Character/RaceController.php
@@ -10,12 +10,15 @@ class RaceController extends Controller
 {
     public function index(Request $request)
     {   
+        $raceArray = [];
         $dwarf = json_decode(file_get_contents('http://dnd5eapi.co/api/races/1'));
         $elf = json_decode(file_get_contents('http://dnd5eapi.co/api/races/2'));
         $halfling = json_decode(file_get_contents('http://dnd5eapi.co/api/races/3'));
         $human = json_decode(file_get_contents('http://dnd5eapi.co/api/races/4'));
         $dragonborn = json_decode(file_get_contents('http://dnd5eapi.co/api/races/5'));
         $gnome = json_decode(file_get_contents('http://dnd5eapi.co/api/races/6'));
+        $tiefling = json_decode(file_get_contents('http://dnd5eapi.co/api/races/9'));
+        array_push($raceArray, $dragonborn, $dwarf, $elf, $gnome, $halfling, $human, $tiefling);
         $character = $request->session()->get('character');
         return view('characters.Form.raceQuest', compact('dwarf', 'elf', 'halfling', 'human', 'dragonborn', 'gnome', 'character'));
     }
