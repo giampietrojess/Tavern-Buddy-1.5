073688092b87e52518bae977867a9d091992c160
Jess Giampietro <giampietrojess@gmail.com>
2018-07-25 03:44:25 -0400

    add alignment images to blade

--- resources/views/characters/Form/alignQuest.blade.php
+++ resources/views/characters/Form/alignQuest.blade.php
@@ -2,11 +2,126 @@
 
 @section('content')
 <div class="container text-center">
-    <h1>What is your Character's Alignment?</h1>
+
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+        
             {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                {{ csrf_field() }}
+
+                <!-- Character Class -->
+                <div class="form-group">
+                    <h1>{{Form::label('What Alignment is your Character?')}}</h1>
+                    <p>
+                        <div class="container">   
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/LawfulGood.png">
+                                <p>
+                                    {{Form::label('Lawful Good')}}
+                                    {{Form::radio('alignment', 'Lawful Good', false, ['class' => 'radio'])}}
+                                </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/NeutralGood.png">
+                                <p>
+                                    {{Form::label('Neutral Good')}}
+                                    {{Form::radio('alignment', 'Neutral Good', false, ['class' => 'radio radio-inline'])}}
+                                </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/ChaoticGood.png">
+                                    <p>
+                                        {{Form::label('Chaotic Good')}}
+                                        {{Form::radio('alignment', 'Chaotic Good', false, ['class' => 'radio radio-inline'])}}
+                                    </p>
+                                </div>
+                                
+                            </div>
+
+                            <div class="row">
+
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/LawfulNeutral.png">
+                                    <p>
+                                        {{Form::label('Lawful Neutral')}}
+                                        {{Form::radio('alignment', 'Lawful Neutral', false, ['class' => 'radio'])}}
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/NeutralNeutral.png">
+                                    <p>
+                                        {{Form::label('True Neutral')}}
+                                        {{Form::radio('alignment', 'True Neutral', false, ['class' => 'radio radio-inline'])}}
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/ChaoticNeutral.png">
+                                    <p>
+                                        {{Form::label('Chaotic Neutral')}}
+                                        {{Form::radio('alignment', 'Chaotic Neutral', false, ['class' => 'radio radio-inline'])}}
+                                    </p>
+                                </div>
+                
+                            </div>
+
+
+                            <div class="row">
+
+                                <div class="col-md-4">
+                                    <img class="img-fluid" class="img-fluid" src="/img/AlignmentImages/LawfulEvil.png">
+                                    <p>
+                                        {{Form::label('Lawful Evil')}}
+                                        {{Form::radio('alignment', 'Lawful Evil', false, ['class' => 'radio'])}}
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/NeutralEvil.png">
+                                    <p>                    
+                                        {{Form::label('Neutral Evil')}}
+                                        {{Form::radio('alignment', 'Neutral Evil', false, ['class' => 'radio radio-inline'])}}
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="/img/AlignmentImages/ChaoticEvil.png">
+                                    <p>
+                                        {{Form::label('Chaotic Evil')}}
+                                        {{Form::radio('alignment', 'Chaotic Evil', false, ['class' => 'radio radio-inline'])}}
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                        
+                    </p>
+
+                    <hr>          
+                </div>
+
+                {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
+            {!! Form::close() !!}
+
+        </div>
+        <div class="col-md-2"></div>
+    
+    
+    
+    
+    
+    
+    
+
+      
+    </div>
+</div>    
+@endsection
+
+
+    
+        <!-- <div class="col-md-3"></div>
+        <div class="col-md-6"> -->
+            <!-- {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
                 {{ csrf_field() }}
                 <div id="carouselExampleControls" class="carousel slide">
                     <div class="carousel-inner" role="listbox">
@@ -14,7 +129,7 @@
                             <div class="row">
                                 <div class="col-md-4"></div>
                                 <div class="col-md-4">
-                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="First slide">
+                                    <img class="img-fluid" src="/img/AlignmentImages/LawfulGood.png" alt="First slide">
                                 </div>
                                 <div class="col-md-4"></div>
                             </div>
@@ -27,7 +142,7 @@
                         <div class="row">
                                 <div class="col-md-4"></div>
                                 <div class="col-md-4">
-                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Second slide">
+                                    <img class="img-fluid" src="/img/AlignmentImages/NeutralGood.png" alt="Second slide">
                                 </div>
                                 <div class="col-md-4"></div>
                             </div>
@@ -40,7 +155,7 @@
                         <div class="row">
                             <div class="col-md-4"></div>
                                 <div class="col-md-4">
-                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Third slide">
+                                    <img class="img-fluid" src="/img/AlignmentImages/ChaoticGood.png" alt="Third slide">
                                 </div>
                                 <div class="col-md-4"></div>
                             </div>
@@ -54,7 +169,7 @@
                             <div class="row">
                                 <div class="col-md-4"></div>
                                 <div class="col-md-4">
-                                    <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Fourth slide">
+                                    <img class="img-fluid" src="/img/AlignmentImages/LawfulNeutral.png" alt="Fourth slide">
                                 </div>
                                 <div class="col-md-4"></div>
                             </div>
@@ -68,7 +183,7 @@
                             <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Fifth slide">
+                                <img class="img-fluid" src="/img/AlignmentImages/NeutralNeutral.png" alt="Fifth slide">
                             </div>
                             <div class="col-md-4"></div>
                             </div>
@@ -82,7 +197,7 @@
                             <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Sixth slide">
+                                <img class="img-fluid" src="/img/AlignmentImages/ChaoticNeutral.png" alt="Sixth slide">
                             </div>
                             <div class="col-md-4"></div>
                             </div>
@@ -96,7 +211,7 @@
                             <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Seventh slide">
+                                <img class="img-fluid" src="/img/AlignmentImages/LawfulEvil.png" alt="Seventh slide">
                             </div>
                             <div class="col-md-4"></div>
                             </div>
@@ -109,7 +224,7 @@
                         <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Eigth slide">
+                                <img class="img-fluid" src="/img/AlignmentImages/NeutralEvil.png" alt="Eigth slide">
                             </div>
                             <div class="col-md-4"></div>
                             </div>
@@ -122,7 +237,7 @@
                         <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <img class="img-fluid" src="/img/ClassIcons/BarbarianIcon.png" alt="Ninth slide">
+                                <img class="img-fluid" src="/img/AlignmentImages/ChaoticEvil.png" alt="Ninth slide">
                             </div>
                             <div class="col-md-4"></div>
                             </div>
@@ -143,10 +258,10 @@
                     </a>
                 </div>
             {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
-            {!! Form::close() !!}      
-        </div>
+            {!! Form::close() !!}       -->
+        <!-- </div>
     
-        <div class="col-md-3"></div>
+        <div class="col-md-3"></div> -->
             
             <!-- {!! Form::open(['action' => 'Character\AlignmentController@alignQuest', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
                 {{ csrf_field() }}
@@ -185,8 +300,3 @@
                 </div>
             {{Form::submit('Next', ['class'=>'btn btn-primary'])}}
         {!! Form::close() !!} -->
-
-      
-    </div>
-</div>    
-@endsection
