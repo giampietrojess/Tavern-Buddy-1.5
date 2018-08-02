9a4c6900620bf21f4d751bf178949a2b101c0084
Jess Giampietro <giampietrojess@gmail.com>
2018-08-02 02:12:46 -0400

    Change color of hamburger menu for mobile responsiveness

--- resources/views/inc/Navbar.blade.php
+++ resources/views/inc/Navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-expand-md navbar-light navbar-laravel">
+<nav class="navbar sticky-top navbar-expand-md navbar-dark navbar-laravel">
             <div class="container">
 
                 <a class="navbar-brand" href="{{ url('/') }}">
